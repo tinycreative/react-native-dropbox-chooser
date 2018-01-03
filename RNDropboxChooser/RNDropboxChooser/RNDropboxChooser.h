42c65782cb0acec59061b6ddb69afe47038e631f
Jamie Faris <jamie@thinkspan.com>
2018-01-03 12:50:40 -0600

    Update React Native imports to work with 0.48
    See https://github.com/facebook/react-native/issues/15775

--- RNDropboxChooser/RNDropboxChooser/RNDropboxChooser.h
+++ RNDropboxChooser/RNDropboxChooser/RNDropboxChooser.h
@@ -7,8 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RCTBridgeModule.h"
-#import "RCTUtils.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUtils.h>
 
 @interface DropboxChooser : NSObject <RCTBridgeModule>
 @end
